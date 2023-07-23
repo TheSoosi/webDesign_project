3d35e6a6f02190c0742aceef86300fc42bf4fd67
Georgi <georgi.khorosh@gmail.com>
2023-07-23 19:31:15 +0300

    added second chart for power of parties by municipality

--- src/app.js
+++ src/app.js
@@ -1,15 +1,48 @@
 import { initMap, setElectionData } from "./map";
-import { loadElectionData } from "./dataLoader";
-import { buildChartPartyVotes } from "./chart";
+import { loadElectionData, loadElectionDataByRegion, initData } from "./dataLoader";
+import { buildChartPartyVotes, buildChartPartiesPower } from "./chart";
 
 export class App{
     async run() {
         await initMap();
+        const municipalityList = await initData();
+        this.initMunicipalitySelect(municipalityList);
         const data = await loadElectionData();
         setElectionData(data);
         buildChartPartyVotes(data);
+        
+        const selectedMunicipality = this.getSelectedMunicipality()
+
+        if (selectedMunicipality) {
+            const data2 = await loadElectionDataByRegion(selectedMunicipality);
+            buildChartPartiesPower(data2);
+        }        
     }
-    
+
+    getSelectedMunicipality() {
+        const select = document.getElementById("municipality-choice");
+        return {
+            municipalityCode: select.value,
+            municipalityName: select.options[select.selectedIndex].text,
+        }
+    }
+
+    initMunicipalitySelect(municipalityList) {
+        const select = document.getElementById("municipality-choice");
+        select.innerHTML = ""
+        municipalityList.forEach(element => {
+        //    select.innerHTML += `<option value="${element.code}">${element.name}</option>`
+            const option = document.createElement("option");
+            option.value = element.code;
+            option.innerText = element.name;
+            select.appendChild(option) 
+        });
+        select.onchange = async () => {
+            const selectedMunicipality = this.getSelectedMunicipality();
+            const data = await loadElectionDataByRegion(selectedMunicipality);
+            buildChartPartiesPower(data);
+        }
+    }
 }
 
 
--- src/chart.js
+++ src/chart.js
@@ -1,6 +1,58 @@
 import { Chart } from "chart.js/auto";
 import { partyColorMapping } from "./map";
 
+let chartPartiesPower = null;
+
+export function buildChartPartiesPower(data) {
+    const labels = data.years;
+    const dataSetList = Object.entries(data.partyVotes).map((entry) => {
+        const [partyName, votes] = entry;
+        return {
+            label: partyName,
+            data: votes,
+            backgroundColor: partyColorMapping[partyName] + "a0",
+        }
+    })
+
+    const chartData = {
+        labels: labels,
+        datasets: dataSetList,
+    }
+
+    const ctx = document.getElementById("chart2-ctx");
+
+    if (chartPartiesPower) {
+        chartPartiesPower.destroy();
+    }
+    
+    chartPartiesPower = new Chart(ctx, {
+        type: "bar",
+        data: chartData,
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+                legend: {
+                    display: true,
+                },
+                title: {
+                    text: "Pary votes over the years, " + data.municipality.municipalityName,
+                    display: true,
+                },
+            },
+            scales: {
+                x: {
+                    stacked: true,
+                },
+                
+                y: {
+                    stacked: true,
+                }
+            }
+        }
+    })
+} 
+
 export function buildChartPartyVotes(data) {
     const backgroundColors = [];
     const partyVotes = data["Mainland"].partyVotes;
